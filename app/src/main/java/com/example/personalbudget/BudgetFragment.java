a52209eed13f185299736da522cbb6ba59b24995
unknown <brunoeiterer@gmail.com>
2020-05-05 20:22:04 -0300

    Add total value to budget fragment

--- app/src/main/java/com/example/personalbudget/BudgetFragment.java
+++ app/src/main/java/com/example/personalbudget/BudgetFragment.java
@@ -12,15 +12,22 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.NumberFormat;
+
 public class BudgetFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle savedInstanceState) {
         View view = layoutInflater.inflate(R.layout.budget_layout, viewGroup, false);
+        BudgetData budgetData = (BudgetData) getArguments().getParcelable("budgetContent");
         RecyclerView budgetRecyclerView = view.findViewById(R.id.budgetRecyclerView);
         budgetRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         BudgetRecyclerViewAdapter budgetRecyclerViewAdapter =
-                new BudgetRecyclerViewAdapter(getActivity(), (BudgetData) getArguments().getParcelable("budgetContent"));
+                new BudgetRecyclerViewAdapter(getActivity(), budgetData);
         budgetRecyclerView.setAdapter(budgetRecyclerViewAdapter);
+
+        TextView totalValueTextView = view.findViewById(R.id.totalValueTextView);
+        totalValueTextView.setText("Total: " + budgetData.getBudgetItem(0).getBudgetItemCurrency().getSymbol() + ' ' +
+                budgetData.getTotalValue().setScale(2).toPlainString());
         return view;
     }
 }
