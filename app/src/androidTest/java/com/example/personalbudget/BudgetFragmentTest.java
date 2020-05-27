a4c4d8e58cc9f7a1564602bc649253a203f72421
unknown <brunoeiterer@gmail.com>
2020-05-27 20:14:55 -0300

    Add AddBudgetItemTest

--- app/src/androidTest/java/com/example/personalbudget/BudgetFragmentTest.java
+++ app/src/androidTest/java/com/example/personalbudget/BudgetFragmentTest.java
@@ -1,19 +1,31 @@
 package com.example.personalbudget;
 
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.uiautomator.UiDevice;
 
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
 
 public class BudgetFragmentTest {
     @Rule
@@ -62,5 +74,31 @@ public class BudgetFragmentTest {
         onView(withId(R.id.addBudgetItemWindowValueEditText)).check(doesNotExist());
         onView(withId(R.id.addBudgetItemWindowDoneButton)).check(doesNotExist());
         onView(withId(R.id.addBudgetItemWindowCancelButton)).check(doesNotExist());
+    }
+
+    @Test
+    public void AddBudgetItemTest() {
+        /* click on AddBudgetItemButton to display the AddBudgetWindow */
+        onView(allOf(withId(R.id.AddBudgetItemButton), isDisplayed())).perform(click());
+
+        /* write date to addBudgetItemWindowDateEditText */
+        String date = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        onView(withId(R.id.addBudgetItemWindowDateEditText)).perform(typeText(date));
+
+        /* write value to */
+        Random random = new Random();
+        DecimalFormat decimalFormat = new DecimalFormat();
+        decimalFormat.setMaximumFractionDigits(2);
+        String value = decimalFormat.format(random.nextFloat() * 500);
+        onView(withId(R.id.addBudgetItemWindowValueEditText)).perform(typeText(value));
+
+        /* click on addBudgetItemWindowDoneButton */
+        onView(allOf(withId(R.id.addBudgetItemWindowDoneButton), isDisplayed())).perform(click());
+
+        /* check if the added item is displayed */
+        onView(allOf(withId(R.id.budgetRecyclerView), isDisplayed())).perform(RecyclerViewActions.scrollTo
+                (allOf(hasDescendant(withText(date)), hasDescendant(withText(containsString(value))))));
+        onView(allOf(withId(R.id.budgetRecyclerView), isDisplayed())).check(matches(allOf(hasDescendant(withText(date)),
+                hasDescendant(withText(containsString(value))))));
     }
 }
